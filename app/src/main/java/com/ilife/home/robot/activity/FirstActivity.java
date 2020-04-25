ae5abbf59bf00ce33a7947db1d58bed47c87eb75
linfei <18565713334@163.com>
2020-04-25 12:00:18 +0800

    1.海外版增加V5x; 2.修改选择语言的位置到SelectCountry之后；

--- app/src/main/java/com/ilife/home/robot/activity/FirstActivity.java
+++ app/src/main/java/com/ilife/home/robot/activity/FirstActivity.java
@@ -20,8 +20,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.aliyun.alink.sdk.bone.plugins.config.BoneConfig;
+import com.aliyun.iot.aep.oa.OALanguageHelper;
 import com.aliyun.iot.aep.sdk.IoTSmart;
 import com.aliyun.iot.aep.sdk._interface.OnAliResponse;
+import com.aliyun.iot.aep.sdk.apiclient.IoTAPIClientImpl;
 import com.aliyun.iot.aep.sdk.contant.IlifeAli;
 import com.aliyun.iot.aep.sdk.framework.AApplication;
 import com.aliyun.iot.aep.sdk.framework.config.GlobalConfig;
@@ -40,6 +43,7 @@ import com.ilife.home.robot.utils.Utils;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 
@@ -140,6 +144,18 @@ public class FirstActivity extends BaseActivity {
                     @Override
                     public void onSuccess(String selectCountry) {
                         MyLogger.d(TAG, "the select country is : " + selectCountry);
+                        Locale locale = Locale.getDefault();
+                        String lan = locale.getLanguage();
+                        if (!lan.equals("zh")) {//
+                            try {
+                                //初始化之后，可以改变显示语言,目前支持中文“zh-CN”, 英文"en-US"，法文"fr-FR",德文"de-DE",日文"ja-JP",韩文"ko-KR",西班牙文"es-ES",俄文"ru-RU"，八种语言
+                                switchLanguage("en-US");
+                                //修改OA多语言 目前支持Locale.US 英文、Locale.SIMPLIFIED_CHINESE 中文、Locale.FRANCE 法语、Locale.JAPAN 日语、Locale.GERMANY 德语、Locale.KOREA 韩语  、new Locale("ru","RU") 俄语、new Locale("es","ES") 西班牙语
+                                OALanguageHelper.setLanguageCode(Locale.US);
+                            } catch (Exception e) {
+
+                            }
+                        }
                         startLogin();
                     }
 
@@ -202,4 +218,18 @@ public class FirstActivity extends BaseActivity {
     }
 
 
+    /**
+     * 改变语言，目前支持中文“zh-CN”, 英文"en-US"，法文"fr-FR",德文"de-DE",日文"ja-JP",韩文"ko-KR",西班牙文"es-ES",俄文"ru-RU"，八种语言
+     *
+     * @param locale: “zh-CN” 或 "en-US"
+     */
+    private void switchLanguage(String locale) {
+        IoTAPIClientImpl.getInstance().setLanguage(locale); // 全局配置，设置后立即起效
+
+        // 容器更改语言
+        BoneConfig.set("language", locale);
+
+        // APIClient更改语言后，push通道重新绑定即可更改push语言
+//        PushManager.getInstance().bindUser();
+    }
 }
